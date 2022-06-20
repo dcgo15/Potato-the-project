333e86a26dd2f76e601a10f7cf08c9d5fa72aa2a
Daniel Gomes <76263577+dcgo15@users.noreply.github.com>
2022-06-20 09:00:42 -0300

    commit all
    reference code

--- teste.py
+++ teste.py
@@ -3,7 +3,7 @@ Sample code so you know how to use thepotato.
 It's quite simple:
 - You import the lib
 - Create a variable
-- Calls the object of your choice
+- Calls the object of your choice : )
 '''
 
 
